94855417cca1a5bb192b0509da0f6d8fe4225542
DevZodiac <devzodiac@protonmail.com>
2023-04-18 22:36:38 +0800

    Deleted.

---
+++ zodiac-test-project/src/pages/api/api.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+res.status(200).json({name: 'POAP API Endpoint'})
+GET /can-mint-shard?address=0x123
+
+// check xDdai
+
+checkPoapEligibility(address)
+
+await axios.post('ozDefender.com/something', {})
+
+WARDEN_WALLET = 0x666
+
+if (ok)
+    proof = signMessage("user 0x123 can mint shard until 12am today", WARDEN_WALLET.private_key)
+    proof, (WARDEN_WALLET.public_key)
+
+    // proof = makeProof(secret, address)
+    return {proof: "proof(address, timestamp)"}
+} else {
+    return not eligible
+}
