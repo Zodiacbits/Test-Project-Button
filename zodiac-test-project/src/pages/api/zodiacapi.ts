2b8df6c6482430fc18b8b781b784f9a1999d3d12
DevZodiac <87861770+Zodiacbits@users.noreply.github.com>
2023-05-29 16:42:00 +0800

    API Key removed

--- zodiac-test-project/src/pages/api/zodiacapi.ts
+++ zodiac-test-project/src/pages/api/zodiacapi.ts
@@ -3,17 +3,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { ethers } from "ethers";
 
 const fetch = require('node-fetch');
-const apiKey = 'X5ljC1hZskOfbf4XmosctiGu5lirqr53Z1ohVmxEN1yhoPY8zFX80If3pvbvwjPmhYyB1h5cSwyMbPc1spR5vO658xVdAe1S7TBD7ffqEIiFOmaU4ZWaCwlaFQMEDjYD';
 const eventID = poapEventId;
 
 fetch('https://api.poap.tech/events/id/${eventId}', {
   headers: {
-    'X-API-KEY': X5ljC1hZskOfbf4XmosctiGu5lirqr53Z1ohVmxEN1yhoPY8zFX80If3pvbvwjPmhYyB1h5cSwyMbPc1spR5vO658xVdAe1S7TBD7ffqEIiFOmaU4ZWaCwlaFQMEDjYD
   }
 })
 ,then(response => response.json())
 ,then(data => {
-  console.log(Data);
+  console.log(data);
 })
 ,catch(Error => {
   console.error(error);
